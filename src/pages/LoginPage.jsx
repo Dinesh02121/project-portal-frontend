5032d30e8034ecd59f1e35287c9659792878e8c2
Dinesh <dineshjangra0212@gmail.com>
2026-01-17 00:39:11 +0530

    New

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -17,7 +17,8 @@ export default function LoginPage() {
     password: ''
   });
 
-  const API_BASE_URL = 'http://localhost:8080';
+  const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:8080';
+
 
   const redirectBasedOnRole = (role) => {
     if (isRedirecting.current) {
